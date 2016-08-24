b71202e11682168960492ab6e53f01db48484ec0
Scott Chesney <swchesney@gmail.com>
2016-08-24 10:05:10 -0700

    add security

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
     
+@login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
     
 def post_detail(request, pk):
@@ -19,6 +23,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
 #    form = PostForm()
 #    return render(request, 'blog/post_edit.html', {'form': form})
@@ -34,6 +39,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})    
 
+@login_required
 def post_edit(request, pk):
     # Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
@@ -49,11 +55,13 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})    
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
