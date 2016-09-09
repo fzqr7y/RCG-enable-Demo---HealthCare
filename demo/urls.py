f893326ca401aa1e760a0502a634c0af208bab4e
Scott Chesney <swchesney@gmail.com>
2016-09-09 15:34:03 -0400

    twilio

--- demo/urls.py
+++ demo/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^member/(?P<pk>\d+)/$', views.member_detail, name='member_detail'),
     # url(r'^member1$', views.member1_detail, name='member1_detail'),
     url(r'^provider/(?P<pk>\d+)/edit/$', views.provider_edit, name='provider_edit'),
-    url(r'^sms/receive$', views.receive_sms, name='receive_sms'),
+    url(r'^sms/receive/$', views.receive_sms, name='receive_sms'),
     url(r'^sms/$', views.sms, name='sms'),
 
     # http://stackoverflow.com/questions/5871730/need-a-minimal-django-file-upload-example
