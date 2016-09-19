4aaceae968e5a1651ce331e45561249d5dfdd737
Scott Chesney <swchesney@gmail.com>
2016-09-19 11:21:02 -0400

    map works

--- demo/urls.py
+++ demo/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     url(r'^members2/$', views.members2, name='members2'),
     url(r'^heartrate2/(?P<pk>\d+)/$', views.heartrate2, name='heartrate2'),
     url(r'^provider_map/(?P<pk>\d+)/$', views.provider_map, name='provider_map'),
+    url(r'^provider_map2/(?P<pk>\d+)/$', views.provider_map2, name='provider_map2'),
+    url(r'^provider_map3/(?P<pk>\d+)/$', views.provider_map3, name='provider_map3'),
     url(r'^provider_members/(?P<pk>\d+)/$', views.provider_members, name='provider_members'),
 
     # http://stackoverflow.com/questions/5871730/need-a-minimal-django-file-upload-example
