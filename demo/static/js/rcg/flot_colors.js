2d5a868c14fb9508a6ca168acbd698bf3735d8d5
Scott Chesney <swchesney@gmail.com>
2016-09-22 09:42:06 -0400

    maps

---
+++ demo/static/js/rcg/flot_colors.js
@@ -0,0 +1,16 @@
+        /* SC: flot */
+        /* chart colors default */
+        var $chrt_border_color = "#efefef";
+        var $chrt_grid_color = "#DDD"
+        var $chrt_main = "#E24913";
+        /* red       */
+        var $chrt_second = "#6595b4";
+        /* blue      */
+        var $chrt_third = "#FF9F01";
+        /* orange    */
+        var $chrt_fourth = "#7e9d3a";
+        /* green     */
+        var $chrt_fifth = "#BD362F";
+        /* dark red  */
+        var $chrt_mono = "#000";
+        /* /SC: flot */
---
+++ demo/static/js/rcg/heartrate.js
@@ -0,0 +1,190 @@
+            /* updating chart */
+            if ($('#updating-chart').length) {
+
+                var initialRun = true;
+                var $on = false;
+                var ajaxComplete = false;
+                var updateInterval = 1000;
+                var plotDuration = 30 * updateInterval;
+                var queryOffset = 5 * updateInterval;
+                var dataOffset = 10 * updateInterval;
+                // var member_id = {{ member.id }};
+                var member_id = $member_id;
+
+                var plotMax, plotMin;
+                var queryMax, queryMin, dataMax;
+                var plotData = [];
+                var queryData = [];
+                var plotOptions;
+                var plot;
+
+                // setup plot
+                function getOptions() {
+                    var options = {
+                        yaxis : {
+                            min : 40, max : 140
+                        },
+                        xaxis: {
+                            min: plotMin, max: plotMax,
+                            mode: "time", timezone: "browser"
+                        },
+                        colors : [$chrt_fourth],
+                        series : {
+                            lines : {
+                                lineWidth : 1,
+                                fill : true,
+                                fillColor : {
+                                    colors : [{
+                                        opacity : 0.4 }, { opacity : 0 }]
+                                },
+                                steps : false
+                            }
+                        }
+                    };
+                    return options;
+                };
+
+var queryCount = 0;
+
+                function getFitbitData() {
+                    // var jdata = []
+                    var t, val, qmin
+                    qmax = (queryData.length>0) ? queryData[queryData.length-1][0] : 0;
+// console.log(qmax)
+                    queryCount++;
+console.log(queryCount);
+                    if (queryCount > 10) {
+                        ajaxComplete = true;
+                    }
+                    $.getJSON("/cloudera/heartrate/get", {
+                            member_id: member_id,
+                            from: (new Date(queryMin)).toISOString(),
+                            to: (new Date(queryMax)).toISOString() },
+                        function(json){
+                            $.each(json, function (index, value) {
+// console.log(value)
+                                t = (new Date(value[0])).getTime();
+                                val = value[1];
+// console.log("qmax: " + qmax + " t: " + t + " val: " + val);
+                                // jdata.push([t, val]);
+                                if (t > qmax) {
+                                    queryData.push([t, val]);
+                                }
+                            });
+                            // queryData = jdata;
+                            ajaxComplete = true;
+                            queryCount = 0;
+                            console.log("queryMin: " + (new Date(queryMin)).toLocaleTimeString() + " queryMax: " + (new Date(queryMax)).toLocaleTimeString() + " queryData.length: " + queryData.length);
+                            console.log("queryData: queryData.length: " + queryData.length + " queryDataMin: " + (new Date(queryData[0][0])).toLocaleTimeString() + " queryDataMax: " + (new Date(queryData[queryData.length-1][0])).toLocaleTimeString());
+                            // console.log("queryData: queryData.length: " + queryData.length + " queryDataMin: " + queryData[0][0] + " queryDataMax: " + queryData[queryData.length-1][0]);
+                            // if (plotData.length > 0) {
+                                // console.log("plotData: plotData.length: " + plotData.length + " plotDataMin: " + plotData[0][0] + " plotDataMax: " + plotData[plotData.length-1][0]);
+                            // }
+                    // console.log("p: " + plotData);
+                    // console.log("q: " + queryData);
+                            $('.heartrate-status').html('');
+                    });
+                    // asynchronous call so no data at this point
+                    return;
+                };
+
+                function setupChart() {
+                    // update the query max time
+                    plotMax = (new Date($.now())).getTime();
+                    plotMin = plotMax - plotDuration;
+                    queryMax = plotMax - queryOffset;
+                    queryMin = (plotData.length > 0) ? plotData[plotData.length - 1][0] : plotMin;
+                    dataMax = queryMax - dataOffset;
+
+                    plotOptions = getOptions();
+                    // console.log("axis: " + plotOptions.xaxis.min + " " + plotOptions.xaxis.max);
+                    // console.log("axis: " + (new Date(plotOptions.xaxis.min)).toLocaleTimeString() + " " + (new Date(plotOptions.xaxis.max)).toLocaleTimeString());
+                    plot = $.plot($("#updating-chart"), [plotData], plotOptions);
+                    plot.draw();
+                    return;
+                }
+
+                function getPlotData() {
+                    // var maxp = 0;
+                    // if (plotData.length > 0) {
+                    //  maxp = plotData[plotData.length-1][0]
+                    //  console.log("maxp: " + maxp)
+                    // }
+                    // while (queryData.length > 0 && queryData[0][0] <= maxp) {
+                    //  console.log("q: " + queryData[0][0])
+                    //  queryData = queryData.slice(1);
+                    // }
+                    while (queryData.length > 0 && queryData[0][0] < dataMax) {
+                        plotData.push(queryData[0])
+                        queryData = queryData.slice(1);
+                    }
+                    if (queryData.length > 0) {
+                    console.log("plotData: " + plotData.length + " " + queryData.length + " dataMax: " + dataMax + " queryData[0][0]: " + queryData[0][0]);
+                    }
+                    // if (plotData.length > 0) {
+                    //  console.log("Query plotData: plotData.length: " + plotData.length + " plotDataMin: " + (new Date(plotData[0][0])).toLocaleTimeString() + " plotDataMax: " + (new Date(plotData[plotData.length-1][0])).toLocaleTimeString());
+                    // }
+                    while (plotData.length > 0 && plotData[0][0] < plotMin - updateInterval) {
+                        plotData = plotData.slice(1);
+                    }
+                    // console.log(plotData);
+           //          if (plotData.length > 0) {
+                    //  console.log("PlotData: plotData.length: " + plotData.length + " plotDataMax: " + (new Date(plotData[0][0])).toLocaleTimeString() + " plotDataMax: " + (new Date(plotData[plotData.length-1][0])).toLocaleTimeString());
+                    // }
+                    if (queryData.length <= 4) {
+                        // console.log("requerying fitbit");
+                        $('.heartrate-status').html('Querying FitBit');
+                        setupChart();
+                        getFitbitData();
+                    }
+                }
+
+                /* live switch */
+                $('input[type="checkbox"]#start_interval').click(function() {
+                    if ($(this).prop('checked')) {
+                        $on = true;
+                        // plotData = [];
+                        queryData = [];
+                        update();
+                    } else {
+                        clearInterval(updateInterval);
+                        $on = false;
+                    }
+                });
+
+                // var updateCount = 0;
+                function update() {
+                    // updateCount++;
+                    // console.log("update start: " + updateCount);
+                    if ($on == true) {
+                        dataMax += updateInterval;
+                        getPlotData();
+                        plot.setData([plotData]);
+                        console.log("update plotted: " + plotData.length + " " + initialRun + " " + ajaxComplete)
+                        // console.log("up axis: " + (new Date(plotOptions.xaxis.min)).toLocaleTimeString() + " " + (new Date(plotOptions.xaxis.max)).toLocaleTimeString());
+                        // if (plotData.length > 0) {
+                        //  console.log("Update: plotData.length: " + plotData.length + " queryData.length: " + queryData.length + " plotDataMin: " + (new Date(plotData[0][0])).toLocaleTimeString() + " plotDataMax: " + (new Date(plotData[plotData.length-1][0])).toLocaleTimeString());
+                        // }
+                        if (plotData.length > 0 && initialRun) {
+                            $on = false;
+                            initialRun = false;
+                        }
+                        if (plotData.length == 0 && ajaxComplete) {
+                            $on = false;
+                            console.log("Query returned no data. user_id:" + member_id + " from: " + (new Date(queryMin)).toLocaleTimeString() + " to: " + (new Date(queryMax)).toLocaleTimeString());
+                            alert("Query returned no data. user_id:" + member_id + " from: " + (new Date(queryMin)).toLocaleTimeString() + " to: " + (new Date(queryMax)).toLocaleTimeString());
+                            $('.heartrate-status').html('');
+                        }
+                        plot.draw();
+                        setTimeout(update, updateInterval);
+                    } else {
+                        clearInterval(updateInterval)
+                    }
+                }
+
+                if (initialRun) {
+                    $on = true
+                    update();
+                }
+            }
+            /*end updating chart*/
